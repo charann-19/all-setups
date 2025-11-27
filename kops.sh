64475be9808a1cf52d04c7d0110d58af155105ac
Charann20050531 <kondrucharanteja761@gmail.com>
2025-11-27 10:23:00 +0530

    Update KOPS_STATE_STORE to new S3 bucket

--- kops.sh
+++ kops.sh
@@ -7,6 +7,6 @@ mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 echo "export PATH=$PATH:/usr/local/bin/" >> source .bashrc
 
-export KOPS_STATE_STORE=s3://kubectlbucketkops.k8s.local
+export KOPS_STATE_STORE=s3://bucket-for-instance-cluster.k8s.local
 kops create cluster --name charan.k8s.local --zones us-east-1a--control-plane-image ami-0fa3fe0fa7920f68e   --control-plane-count=1 --control-plane-size c7i-flex.large --image ami-0fa3fe0fa7920f68e    --node-count=2 --node-size c7i-flex.large
 kops update cluster --name charan.k8s.local --yes --admin
